67d2ca8c444e2f2eb28e766bf871f9cb52699b77
snzyco <42591881+snzyco@users.noreply.github.com>
2019-02-11 21:29:55 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="utf-8">
+     <title>sean knight compsci 192 homepage</title>
+</head>
+
+<body>
+     <h1>lab assn. no. 02</h1>
+     <br>
+     <?php
+          $company_name = "Ambivalor Self-Driving Cars";
+          $company_address = "1742 Heavily Medicated Crescent";
+          $company_citystatezip = "Palmdale, CA 93551";
+     ?>
+     <h2><?php print $company_name; ?></h2>
+     <?php
+          print company_address . ", " . company_citystatezip;
+     ?>
+     <br><br><br>
+     <h1>lab assn. no. 01</h1>
+     <br>
+     <?php
+          print "My name is sean harlan knight";
+     ?>
+</body>
+</html>
